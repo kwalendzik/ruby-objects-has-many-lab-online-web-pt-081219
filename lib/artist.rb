d1468f7739caf911ecbc69a601b7586b0901a06b
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-09-29 14:22:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,25 +1,29 @@
-class Artist
- attr_accessor :name
- 
- def initialize(name)
-   @name = name
- end
- 
- def add_song(song)
-   song.artist = self
- end
- 
- def songs
-   Song.all.select {|song| song.artist == self}
- end
- 
- def add_song_by_name(name, genre)
-   song = Song.new(name, genre)
-   add_song(song)
- end
- 
- def song_count
-   Song.all.count
- end
- 
-end
+require 'pry'
+
+class Artist 
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name 
+        @songs = []
+    end 
+
+    def add_song(song)
+        @songs << song 
+        song.artist = self 
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end 
+
+    def add_song_by_name(name)
+        song = Song.new(name)
+        song.artist = self 
+    end 
+    
+    def self.song_count 
+        Song.all.count 
+    end 
+
+end 
